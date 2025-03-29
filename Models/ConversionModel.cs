9be72124a055a2793c7059022ba358735fc38dd4
Smith <waltersmithchv@gmail.com>
2025-03-28 22:48:35 -0500

    Finalizando merge de Convercion

--- Models/ConversionModel.cs
+++ Models/ConversionModel.cs
@@ -2,8 +2,8 @@ namespace CasaCambio.Models
 {
     public class ConversionModel
     {
-        public string MonedaOrigen { get; set; }
-        public string MonedaDestino { get; set; }
+        public required string MonedaOrigen { get; set; }
+        public required string MonedaDestino { get; set; }
         public decimal Cantidad { get; set; }
         public decimal TipoCambio { get; set; }
         public decimal Resultado => Cantidad * TipoCambio;
